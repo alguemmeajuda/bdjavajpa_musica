875dea70b1d7054d0a75d9b273606d46a5d5a637
alguemmeajuda <124810553+alguemmeajuda@users.noreply.github.com>
2024-09-01 21:56:15 -0300

    Add files via upload

---
+++ BdjavajpaMusica/src/vo/Musica.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vo;
+
+import java.util.Calendar;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Musica {
+    @Id
+    @GeneratedValue
+    private int codigo = 0;
+    private String titulo;
+    private String interprete;
+    private String letra;
+
+    /**
+     * @return the codigo
+     */
+    public int getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the interprete
+     */
+    public String getInterprete() {
+        return interprete;
+    }
+
+    /**
+     * @param interprete the interprete to set
+     */
+    public void setInterprete(String interprete) {
+        this.interprete = interprete;
+    }
+
+    /**
+     * @return the letra
+     */
+    public String getLetra() {
+        return letra;
+    }
+
+    /**
+     * @param letra the letra to set
+     */
+    public void setLetra(String letra) {
+        this.letra = letra;
+    }
+}
